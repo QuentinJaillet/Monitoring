b88bd083c1b9380e5767a1f9587cc0f0583a5ee0
Quentin JAILLET <qjaillet.prestataire@clauger.fr>
2025-04-14 10:20:07 +0200

    Enhance GetBooksQueryHandler to include author information in book retrieval

--- ServiceB/Application/GetBooks/GetBooksQueryHandler.cs
+++ ServiceB/Application/GetBooks/GetBooksQueryHandler.cs
@@ -9,17 +9,37 @@ public class GetBooksQueryHandler : IRequestHandler<GetBooksQuery, IList<BookVie
 {
     private ILogger<GetBooksQueryHandler> _logger;
     private readonly IBookRepository _bookRepository;
+    private readonly IAuthorRepository _authorRepository;
 
-    public GetBooksQueryHandler(ILogger<GetBooksQueryHandler> logger, IBookRepository bookRepository)
+    public GetBooksQueryHandler(ILogger<GetBooksQueryHandler> logger, IBookRepository bookRepository, IAuthorRepository authorRepository)
     {
         _logger = logger;
         _bookRepository = bookRepository;
+        _authorRepository = authorRepository;
     }
 
     public async Task<IList<BookViewModel>> Handle(GetBooksQuery request, CancellationToken cancellationToken)
     {
         _logger.LogInformation("Get books");
-        var books = await _bookRepository.GetBooksAsync(cancellationToken).ConfigureAwait(false);
+        
+        // Get authors to populate the book's author
+        var authors = await _authorRepository
+            .GetAuthorsAsync(cancellationToken)
+            .ConfigureAwait(false);
+        
+        // Get books
+        var books = await _bookRepository
+            .GetBooksAsync(cancellationToken)
+            .ConfigureAwait(false);
+        
+        // Populate the book's author
+        foreach (var book in books)
+        {
+            var author = authors.FirstOrDefault(a => a.Id == book.AuthorId);
+            if (author != null)
+                book.Author = author;
+        }
+        
         return books.ToViewModel();
     }
 }
